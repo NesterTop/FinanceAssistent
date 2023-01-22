9fbd6256dadd2a1fa0389a4b8e0bdd0ec8e3d13f
Андрей Соловов <solovov2003@yandex.ru>
2023-01-22 19:08:23 +0300

    Добавьте файлы проекта.

---
+++ FinanceAssistent/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace FinanceAssistent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connectionString = @"Data Source=DESKTOP-AVGELME\STP; Initial Catalog=DataBase; Integrated Security=True";
+
+            DataBaseContext dataBaseContext = new DataBaseContext(connectionString);
+
+            dataBaseContext.Open();
+
+            
+
+            dataGridView1.DataSource = dataBaseContext.TipDohoda;
+        }
+    }
+}
